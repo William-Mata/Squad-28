628b29576d611c928ea75c4a9fafc3e94249717a
William Mata <william.xavante@gmail.com>
2021-12-29 18:23:07 -0300

    Imagens
    Consegui armazenar a imagem no banco de dados agora e ver para manipular ela.

---
+++ Tribo/Tribo/Migrations/20211229211925_teste.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tribo.Migrations
+{
+    public partial class teste : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contato",
+                columns: table => new
+                {
+                    IdContato = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mensagem = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contato", x => x.IdContato);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tribo",
+                columns: table => new
+                {
+                    IdTribo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NomeTribo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tribo", x => x.IdTribo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pacote",
+                columns: table => new
+                {
+                    IdPacote = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Valor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataFim = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IdImagem = table.Column<int>(type: "int", nullable: false),
+                    IdTribo = table.Column<int>(type: "int", nullable: false),
+                    TriboParceiraIdTribo = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacote", x => x.IdPacote);
+                    table.ForeignKey(
+                        name: "FK_Pacote_Tribo_TriboParceiraIdTribo",
+                        column: x => x.TriboParceiraIdTribo,
+                        principalTable: "Tribo",
+                        principalColumn: "IdTribo");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    IdCliente = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Pacote = table.Column<int>(type: "int", nullable: false),
+                    pacoteIdPacote = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.IdCliente);
+                    table.ForeignKey(
+                        name: "FK_Cliente_Pacote_pacoteIdPacote",
+                        column: x => x.pacoteIdPacote,
+                        principalTable: "Pacote",
+                        principalColumn: "IdPacote");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Imagem",
+                columns: table => new
+                {
+                    IdImg = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Dados = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    ContentType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PacoteIdPacote = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imagem", x => x.IdImg);
+                    table.ForeignKey(
+                        name: "FK_Imagem_Pacote_PacoteIdPacote",
+                        column: x => x.PacoteIdPacote,
+                        principalTable: "Pacote",
+                        principalColumn: "IdPacote");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Viagem",
+                columns: table => new
+                {
+                    IdViagem = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Origem = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Destino = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Valor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataIda = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataVolta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PessoaIdCliente = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Viagem", x => x.IdViagem);
+                    table.ForeignKey(
+                        name: "FK_Viagem_Cliente_PessoaIdCliente",
+                        column: x => x.PessoaIdCliente,
+                        principalTable: "Cliente",
+                        principalColumn: "IdCliente",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cliente_pacoteIdPacote",
+                table: "Cliente",
+                column: "pacoteIdPacote");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imagem_PacoteIdPacote",
+                table: "Imagem",
+                column: "PacoteIdPacote");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacote_TriboParceiraIdTribo",
+                table: "Pacote",
+                column: "TriboParceiraIdTribo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Viagem_PessoaIdCliente",
+                table: "Viagem",
+                column: "PessoaIdCliente");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contato");
+
+            migrationBuilder.DropTable(
+                name: "Imagem");
+
+            migrationBuilder.DropTable(
+                name: "Viagem");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "Pacote");
+
+            migrationBuilder.DropTable(
+                name: "Tribo");
+        }
+    }
+}
