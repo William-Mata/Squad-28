5cd71b6ab806c812fd2ac3f3d1127ed964ca616e
William Mata <william.xavante@gmail.com>
2021-12-29 18:18:12 -0300

    Imagens

---
+++ Tribo/Tribo/Controllers/ImagensController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tribo.Models;
+
+namespace AspNetCore_EnviaExibeImagem.Controllers
+{
+    public class ImagensController : Controller
+    {
+    
+        private TriboDbContext _context;
+
+        public ImagensController(TriboDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<int> imagens = _context.Imagens.Select(m => m.IdImg).ToList();
+            return View(imagens);
+        }
+
+        [HttpPost]
+        public IActionResult UploadImagem(IList<IFormFile> arquivos)
+        {
+            IFormFile imagemEnviada = arquivos.FirstOrDefault();
+            if (imagemEnviada != null || imagemEnviada.ContentType.ToLower().StartsWith("image/"))
+            {
+                MemoryStream ms = new MemoryStream();
+                imagemEnviada.OpenReadStream().CopyTo(ms);
+
+                Imagem imagemEntity = new Imagem()
+                {
+                    Nome = imagemEnviada.Name,
+                    Dados = ms.ToArray(),
+                    ContentType = imagemEnviada.ContentType
+                };
+                _context.Imagens.Add(imagemEntity);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public FileStreamResult VerImagem(int id)
+        {
+            Imagem imagem = _context.Imagens.FirstOrDefault(m => m.IdImg == id);
+            MemoryStream ms = new MemoryStream(imagem.Dados);
+            return new FileStreamResult(ms, imagem.ContentType);
+        }
+    }
+}
